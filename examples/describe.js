508c522d1ee8d01fbc36ef783b87a287ebf81b82
bergi <bergi@axolotlfarm.org>
2020-09-04 02:25:47 +0200

    added support for describe and union

---
+++ examples/describe.js
@@ -0,0 +1,29 @@
+/*
+
+  This example builds a query that describes the resources in the variable defined by a union triple pattern.
+
+*/
+const rdf = require('@rdfjs/data-model')
+const namespace = require('@rdfjs/namespace')
+const sparql = require('..')
+
+const ns = {
+  ex: namespace('http://example.org/'),
+  rdf: namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+}
+
+const observation = rdf.variable('observation')
+
+const query = sparql.describe([observation])
+  .where([
+    sparql.union([
+      [
+        [observation, ns.rdf.type, ns.ex.Observation]
+      ], [
+        [observation, ns.rdf.type, ns.ex.ObservationSet]
+      ]
+    ])
+  ])
+  .limit(100)
+
+console.log(query.toString())
