83c8bfb2002b581a0d6e26700311a8b2f05da18e
Benjamin Hofstetter <benjaminhofstetter@mobi.ch>
2020-09-29 14:22:21 +0200

    loop example

---
+++ examples/loop.js
@@ -0,0 +1,33 @@
+/*
+
+  This example builds a query ussing a loop.
+
+*/
+const rdf = require("@rdfjs/data-model");
+const namespace = require("@rdfjs/namespace");
+const sparql = require("..");
+
+const ns = {
+  ex: namespace("http://example.org/"),
+  rdf: namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+};
+
+const observation = rdf.variable("observation");
+const date = rdf.variable("date");
+const temperature = rdf.variable("temperature");
+
+const someArray = [
+  [observation, ns.rdf.type, ns.ex.Observation],
+  [observation, ns.ex.date, date],
+  [observation, ns.ex.temperature, temperature],
+];
+
+const unions = someArray.map((triples) => {
+  return [sparql.select(["*"]).where([triples]).limit(10)];
+});
+
+const query = sparql
+  .select([observation, date, temperature])
+  .where([sparql.union(unions)]);
+
+console.log(query.toString());
